1409072ba5778b181abe5f499e64f815018c927d
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2019-08-02 23:25:57 +0600

    lecture 15

--- app.ts
+++ app.ts
@@ -1,4 +1,35 @@
-let myName = "shakil";
-let no: number;
-no = 1;
+// let myName = "shakil";
+// let no: number;
+// no = 1;
+//
+// // array
+// let hi = "ok";
+// let hobbies: any = ['play', 'pro'];
+//
+// console.log(hobbies[0]);
+// console.log(typeof hobbies);
+// hobbies = [32];
 
+// tuples
+
+// let tuples: [string, number] = ["shakil", 1812966642];
+
+// enum
+// enum Collor {
+//     RED,
+//     GREEN,
+//     YEOLLOW =100,
+//     WHITE
+// }
+//
+// let collor: Collor = Collor.WHITE;
+// console.log(collor);
+
+// anyType
+//
+// let car: any = "bmw";
+// console.log(car);
+//
+// // object
+// car = {Model: "bmw", Serise: 20};
+// console.log(car);
